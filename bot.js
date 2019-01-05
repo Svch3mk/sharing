4a1b24ddd6078501f2389cfcaa32cdf5dbc7cd76
Svch3mk <45947938+Svch3mk@users.noreply.github.com>
2019-01-05 23:43:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const jimp = require('jimp')
 const math = require('math-expression-evaluator'); 
 const child_process = require("child_process");
 const Canvas = require('canvas');
-var prefix = "/"
+var prefix = "^"
 const client = new Discord.Client();
 const agree = "✅";
 const disagree = "❌";
@@ -36,184 +36,13 @@ client.on('ready', () => {
   
 });
 
-let cooldown = new Set();
-let sec = 60;
-var ticketc = 0000;
 
-client.on('guildMemberAdd', async member => {
-	member.guild.roles.find(role => role.name === "| CF | Member");
-	const channel = member.guild.channels.find(ch => ch.name === 'welcome');
-	if (!channel) return;
 
-	const canvas = Canvas.createCanvas(700, 250);
-	const ctx = canvas.getContext('2d');
-
-	const background = await Canvas.loadImage('https://cdn.discordapp.com/attachments/515879836309520403/515882876605169665/wallpaper.jpg');
-	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-	ctx.strokeStyle = '#74037b';
-	ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-	// Slightly smaller text placed above the member's display name
-	ctx.font = '28px sans-serif';
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
-
-	// Add an exclamation point here and below
-	ctx.font = applyText(canvas, `${member.displayName}!`);
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
-
-	ctx.beginPath();
-	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-	ctx.closePath();
-	ctx.clip();
-
-	const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
-	const avatar = await Canvas.loadImage(buffer);
-	ctx.drawImage(avatar, 25, 25, 200, 200);
-
-	const attachment = new Discord.Attachment(canvas.toBuffer(), 'https://cdn.discordapp.com/attachments/515879836309520403/515882914869805056/Welcome.png');
-
-	channel.send(`Welcome To CrossFire, ${member}!`, attachment);
-});
 client.on('message', async message => {
 	    let args = message.content.split(' ').slice(1).join(' ');
     
       
-     if (message.content.startsWith(prefix + "clear")) {
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **ليس لديك صلاحيات**');
-        var msg;
-        msg = parseInt();
-     
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-      message.channel.sendMessage("", {embed: {
-        title: "Done | تــم",
-        color: 0x06DF00,
-        description: "تم مسح الرسائل بنجاح",
-        footer: {
-          text: "Name Bot."
-        }
-      }}).then(msg => {msg.delete(3000)});
-                          }
  
-
-	
-if (message.content.startsWith(prefix + "new")) {
-
-                        if(cooldown.has(message.author.id)){
-        message.delete();
-        message.channel.send(`عليك ان تنتظر 1 دقيقة لأنشاء تذكرة مرة اخرى`).then((mxd) => {
-		        mxd.delete(5000);
-	});
-        message.delete();
-    }else{
-        const reason = message.content.split(" ").slice(1).join(" ");   
-        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);   
-        message.guild.createChannel(`ticket-${ticketc}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "Support Team");
-            let role2 = message.guild.roles.find("name", "@everyone");
-            c.overwritePermissions(role, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });  
-            c.overwritePermissions(role2, {
-                SEND_MESSAGES: false,
-                READ_MESSAGES: false
-            });
-            c.overwritePermissions(message.author, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });
-          
-	message.channel.send(`:white_check_mark: **تم إنشاء تذكرتك ، #${c.name}.**`);
-        if(!reason){
-		const embed = new Discord.RichEmbed()
-                .setColor(0xCF40FA)
-	  	.addField(`Created By : ` , `${message.author.username}`)
-                .addField(`مرحباّ ${message.author.username}!`, `يرجي شرح سبب فتح هذا التذكرة . سيكون فريق الدعم لدينا قريبا للمساعدة`)
-                .addField(`Subject :` , `None`)
-	    	.setTimestamp();
-            c.send({
-                embed: embed
-            });
-	}else {
-				const embedd = new Discord.RichEmbed()
-                .setColor(0xCF40FA)
-	  	.addField(`Created By : ` , `${message.author.username}`)
-                .addField(`مرحباّ ${message.author.username}!`, `يرجي شرح سبب فتح هذا التذكرة . سيكون فريق الدعم لدينا قريبا للمساعدة`)
-                .addField(`Subject :` , `${reason}`)
-	    	.setTimestamp();
-            c.send({
-                embed: embedd
-            });
-	}
-            ticketc++;
-		    cooldown.add(message.author.id);
-    setTimeout(() => {
-        cooldown.delete(message.author.id);
-    }, sec * 1000);
-      
-
-    })
-}
-}
-
-	
-if(message.content.startsWith(prefix + "ban")){
-if(!message.member.hasPermission("BAN_MEMBERS")){
-	message.channel.send(`**BAN_MEMBERS : ليس لديك صلاحية  ** `);
-
-}else {
-  let mban = message.mentions.members.first();
-  let reason = args.slice(1).join(" ");
-if(!mban) {
-  message.channel.send(`** منشن الشخص **`).then((metionm) => {
-    metionm.delete(5000);
-  });
-}else if(!reason){
-  message.channel.send(`** اكتب السبب**`).then((metionm2) => {
-    metionm2.delete(5000);
-  });
-
-
-}else {
-  message.channel.send(`** ${mban} , Banned from ${message.guild.name} server :airplane: **`).then((banm) => {
-    banm.delete(5000);
-  });
-  message.guild.member(mban).ban(7, mban);
-}
-}
-}
-	
-
-if (message.content.startsWith(prefix + "close")) {
-    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
-
-    message.channel.send(`هل أنت متأكد؟ بعد التأكيد ، لا يمكنك عكس هذا الإجراء!\n للتأكيد ، اكتب\`^close\`. سيؤدي ذلك إلى مهلة زمنية في غضون 10 ثوان وإلغائها`)
-        .then((m) => {
-            message.channel.awaitMessages(response => response.content === '^close', {
-                    max: 1,
-                    time: 10000,
-                    errors: ['time'],
-                })   
-                .then((collected) => {
-                    message.channel.delete();
-                })  
-                .catch(() => {
-                    m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-                        m2.delete();
-                    }, 3000);
-                });
-        });
-}
-
-
-
-
-
-	
    let embed = new Discord.RichEmbed()
 
 
@@ -304,52 +133,6 @@ if (message.content.startsWith(prefix + "close")) {
     
         
      
-
-
-
-    if(message.content.startsWith(prefix + "help")){
-        message.channel.send(agree);
-        message.author.send(`
-        ╭━━━╮╱╱╱╱╱╱╱╱╱╱╭━━━╮
-        ┃╭━╮┃╱╱╱╱╱╱╱╱╱╱┃╭━━╯
-        ┃┃╱╰╋━┳━━┳━━┳━━┫╰━━┳┳━┳━━╮
-        ┃┃╱╭┫╭┫╭╮┃━━┫━━┫╭━━╋┫╭┫┃━┫
-        ┃╰━╯┃┃┃╰╯┣━━┣━━┃┃╱╱┃┃┃┃┃━┫
-        ╰━━━┻╯╰━━┻━━┻━━┻╯╱╱╰┻╯╰━━╯
-        
-      **Soon, our bot will be translated to English**
-
-      **  ⦁~~                                     ~~⦁**
-
-        **  العامةالأدراية**
-
-        **/bc** ارسال رسالة الى جميع الأعضاء .
-        **/ban** لحظر شخص من السيرفر لمدة 7 ايام .
-        **/new** لأنشاء تذكرة في السيرفر .
-        **/close** لإغلاق التذكرة .
-        **/clear** للـحذف الشات .
-     
-        
-        
-        `);
-    }
-
-    if(message.content.startsWith(prefix + "ping")){
-
-        if(!message.member.hasPermission("ADMINISTRATOR")){
-
-              message.channel.send(`ليس لديك صلاحية`);
-              message.delete;
-
-
-        }else{
-   
-                    message.channel.send(client.ping + ` : سرعة البوت هي  `);
-        }
-    }
-
- 
-	
     
 
 })
